3ac9b2e361ca54c9c4af3163a8ac79b7bb051112
xxxx-Bella <hyx18390659623@163.com>
2023-08-06 17:15:25 +0800

    add annotation to the whole part

--- buffer.py
+++ buffer.py
@@ -71,31 +71,33 @@ def main(args):
     ##　创建损失函数
     criterion = nn.CrossEntropyLoss().to(args.device)
 
-    trajectories = []
+    trajectories = []  # 存储训练过程中模型的参数变化
 
-    dst_train = TensorDataset(copy.deepcopy(images_all.detach()), copy.deepcopy(labels_all.detach()))
-    trainloader = torch.utils.data.DataLoader(dst_train, batch_size=args.batch_train, shuffle=True, num_workers=0)
+    dst_train = TensorDataset(copy.deepcopy(images_all.detach()), copy.deepcopy(labels_all.detach()))  #　创建一个新的数据集，复制images_all和labels_all，detach()返回的tensor和原始tensor共同一个内存，但是该tensor的requires_grad永远为false
+    trainloader = torch.utils.data.DataLoader(dst_train, batch_size=args.batch_train, shuffle=True, num_workers=0)  # 数据加载器，将数据分成批次进行训练
 
+    ## 数据集扩充
     ''' set augmentation for whole-dataset training '''
-    args.dc_aug_param = get_daparam(args.dataset, args.model, args.model, None)
-    args.dc_aug_param['strategy'] = 'crop_scale_rotate'  # for whole-dataset training
+    args.dc_aug_param = get_daparam(args.dataset, args.model, args.model, None)  # 数据增强参数
+    args.dc_aug_param['strategy'] = 'crop_scale_rotate'  # for whole-dataset training 设置数据增强策略：裁剪，缩放，旋转
     print('DC augmentation parameters: \n', args.dc_aug_param)
 
-    ## 模型训练循环
+    ## 模型训练循环，每次迭代都会训练一个合成数据模型
     for it in range(0, args.num_experts):
         # 训练合成数据
         ''' Train synthetic data '''
         teacher_net = get_network(args.model, channel, num_classes, im_size).to(args.device)  # get a random model
-        teacher_net.train()
+        teacher_net.train()  # 设置为训练模式，从而启用模型的训练特性，如Dropout等
         lr = args.lr_teacher
-        teacher_optim = torch.optim.SGD(teacher_net.parameters(), lr=lr, momentum=args.mom, weight_decay=args.l2)  # optimizer_img for synthetic data
-        teacher_optim.zero_grad()
+        teacher_optim = torch.optim.SGD(teacher_net.parameters(), lr=lr, momentum=args.mom, weight_decay=args.l2)  # optimizer_img for synthetic data 创建一个随机梯度下降（SGD）优化器，用于更新模型参数
+        teacher_optim.zero_grad()  # 梯度置零
 
         timestamps = []  # 记录训练过程中的参数变化（时间戳）
-        timestamps.append([p.detach().cpu() for p in teacher_net.parameters()])
+        timestamps.append([p.detach().cpu() for p in teacher_net.parameters()])  # 记录当前模型的参数，以便在训练过程中跟踪参数变化
 
-        lr_schedule = [args.train_epochs // 2 + 1]
-
+        lr_schedule = [args.train_epochs // 2 + 1]  # 学习率调整的时间表，在训练的某些特定 epoch 时，将会减小学习率
+        
+        # 模型训练的主要循环，调用utils.py的epoch函数
         for e in range(args.train_epochs):
 
             train_loss, train_acc = epoch("train", dataloader=trainloader, net=teacher_net, optimizer=teacher_optim,
@@ -105,24 +107,26 @@ def main(args):
                                         criterion=criterion, args=args, aug=False)
 
             print("Itr: {}\tEpoch: {}\tTrain Acc: {}\tTest Acc: {}".format(it, e, train_acc, test_acc))
-
+            
+            # record model parameters of each epoch
             timestamps.append([p.detach().cpu() for p in teacher_net.parameters()])
-
-            if e in lr_schedule and args.decay:
-                lr *= 0.1
-                teacher_optim = torch.optim.SGD(teacher_net.parameters(), lr=lr, momentum=args.mom, weight_decay=args.l2)
-                teacher_optim.zero_grad()
+            
+            # 学习率衰减
+            if e in lr_schedule and args.decay:  # args.decay=True 启用了学习率衰减
+                lr *= 0.1  # 减小到当前 lr 的1/10
+                teacher_optim = torch.optim.SGD(teacher_net.parameters(), lr=lr, momentum=args.mom, weight_decay=args.l2)  # 重新创建一个 SGD 优化器，以应用新的学习率
+                teacher_optim.zero_grad()  # 将优化器的梯度置零，为下一个 epoch 的梯度计算做准备
 
         trajectories.append(timestamps)
 
         # 参数保存
         if len(trajectories) == args.save_interval:
             n = 0
-            while os.path.exists(os.path.join(save_dir, "replay_buffer_{}.pt".format(n))):
+            while os.path.exists(os.path.join(save_dir, "replay_buffer_{}.pt".format(n))):  # 找到一个未存在的文件名后缀编号n
                 n += 1
             print("Saving {}".format(os.path.join(save_dir, "replay_buffer_{}.pt".format(n))))
-            torch.save(trajectories, os.path.join(save_dir, "replay_buffer_{}.pt".format(n)))
-            trajectories = []
+            torch.save(trajectories, os.path.join(save_dir, "replay_buffer_{}.pt".format(n)))  #文件名：replay_buffer_n.pt，文件格式：PyTorch格式的二进制文件
+            trajectories = []  # 清空
 
 
 if __name__ == '__main__':
@@ -145,7 +149,7 @@ if __name__ == '__main__':
     parser.add_argument('--decay', action='store_true')
     parser.add_argument('--mom', type=float, default=0, help='momentum')
     parser.add_argument('--l2', type=float, default=0, help='l2 regularization')
-    parser.add_argument('--save_interval', type=int, default=10)
+    parser.add_argument('--save_interval', type=int, default=10, help='trajectories num of each save opt')
 
     args = parser.parse_args()
     main(args)
